75f839dac0cddeee9c096e68ae49969600b72b26
Ormorod <Adam.Ormondroyd@gmail.com>
2022-12-04 20:10:10 +0000

    tidy up day1a

--- day1/day1a.f90
+++ day1/day1a.f90
@@ -1,20 +1,18 @@
-program day1
+program day1a
     implicit none
     integer :: largest
     integer :: running_total
     integer :: buffer
     character(len=20) :: buffer_string
-    integer :: i
 
+    running_total=0
     largest=0
 
-    print *, "Hello day1!"
-    ! open(unit=1,file="test_input.txt")
-    open(unit=1,file="input.txt")
+    open(unit=1,file="test_input.txt")
+    ! open(unit=1,file="input.txt")
 
-    do i=1, 2244
-    read (1,'(a)',end=101) buffer_string
-        print *,buffer_string
+    do
+        read (1,'(a)',end=101) buffer_string
         if (buffer_string.eq."") then
             if (running_total.gt.largest) then
                 largest=running_total
@@ -22,14 +20,16 @@ program day1
             running_total=0
         else
             read(buffer_string,*) buffer
-            print *,buffer
             running_total = running_total + buffer
         end if
-        print *, running_total
     end do
     101 continue
-    print *, largest
+    if (running_total.gt.largest) then
+        largest=running_total
+    end if
+
+    print *,largest
     close(1)
 
 
-end program day1
+end program day1a
