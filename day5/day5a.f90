8b11c004eee5ed5705deaa6b93c85a86deec3059
Ormorod <Adam.Ormondroyd@gmail.com>
2022-12-07 18:55:37 +0000

    day5b - lmao I accidentally answered this first, so part a is just commented out

--- day5/day5a.f90
+++ day5/day5a.f90
@@ -41,6 +41,8 @@ program day5a
     character(len=256) :: buffer
     integer :: i, ii
     integer :: a,b,c
+
+
     ! open(unit=1,file="example_input.txt")
     open(unit=1,file="input.txt")
     ii = 1
@@ -55,42 +57,35 @@ program day5a
     end do
     print *,"finished reading file"
     do i=1, size(stacks)
-        ! stacks(i) = reverse(stacks(i))
-        print *,stacks(i)
-        print *,len(stacks(i))
         stacks(i)=adjustl(stacks(i))
+        print *,stacks(i)
     end do 
-
-    print *,len(stacks(2))
-    print *,len_trim(stacks(2))
-    print *, len_trim(trim(stacks(1))//trim(stacks(2)))
-    stacks(1) = trim(stacks(1))//trim(stacks(2))
-    print *,len(stacks(1))
 
     ! get rid of blank line
     read(1,'(A)')buffer
-    print *,buffer
+
+    print *,'---------------------------------------------'
 
     do
         read(1,'(A)',end=101)buffer
         call get_three_ints_from_string(buffer,a,b,c)
-        print *,a,b,c
-        ! stacks(c) = trim(stacks(b)(1:a))//trim(stacks(c))
-        ! stacks(b) = trim(stacks(b)(a+1:256))
-        ! would have to reverse - might as well move one at a time
-        do i=1,a
-            stacks(c) = trim(stacks(b)(1:1))//stacks(c)
-            stacks(b) = stacks(b)(2:256)
-        end do
 
-        do i=1, size(stacks)
-            print *,stacks(i)
-        end do 
+        ! part a)
+        ! do i=1,a
+            ! stacks(c) = trim(stacks(b)(1:1))//stacks(c)
+            ! stacks(b) = stacks(b)(2:256)
+        ! end do
+        ! part b)
+        stacks(c) = trim(stacks(b)(1:a))//trim(stacks(c))
+        stacks(b) = trim(stacks(b)(a+1:256))
+
     end do
 
-
     101 continue
-
-
     close(unit=1)
+
+    do i=1, size(stacks)
+        print *,stacks(i)
+    end do 
+
 end program day5a
