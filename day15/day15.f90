cb83b708a3b8e951b3753e587a21495513085f95
Ormorod <Adam.Ormondroyd@gmail.com>
2022-12-15 19:24:45 +0000

    day 15b, can't bring myself to change the integer types to make the result print properly

--- day15/day15.f90
+++ day15/day15.f90
@@ -49,14 +49,17 @@ program day15
     implicit none
     character(len=256) :: buffer
     integer :: num_sensors
-    integer :: i, ii, num_cannot
+    integer :: i, ii, iii, num_cannot
     logical :: cannot
     integer, dimension(:,:), allocatable :: sensor_and_beacon
     integer, dimension(:), allocatable :: distances
+    integer, dimension(:,:), allocatable :: sensor
 
     integer, dimension(2) :: x
+    integer :: r
     integer :: manhattan
     logical :: is_beacon
+    logical :: is_distress
     open(unit=1,file="input.txt")
     num_sensors = 0
     do
@@ -72,12 +75,13 @@ program day15
         read(1,'(A)')buffer
         call read_line(buffer, sensor_and_beacon(i,:))
     end do
+    close(1)
 
     do i=1, num_sensors
         distances(i) = manhattan(sensor_and_beacon(i,1:2),sensor_and_beacon(i,3:4))
     end do
 
-    x(2) = 2000000
+    x(2) = 10
     print *,minval(sensor_and_beacon(:,1))-maxval(distances)
     print *,maxval(sensor_and_beacon(:,1))+maxval(distances)
     do ii=minval(sensor_and_beacon(:,1))-maxval(distances),maxval(sensor_and_beacon(:,1))+maxval(distances)
@@ -95,7 +99,49 @@ program day15
 
     print *,"-------------------------------------------------------------------------"
     print *,num_cannot
+    print *,"-------------------------------------------------------------------------"
 
+    allocate(sensor(num_sensors,2))
+    sensor = sensor_and_beacon(:,1:2)
+    do iii=1,num_sensors
+        r = distances(iii)+1
+        print *, sensor(iii,:)
+        print *, r
+        x(1) = sensor(iii,1)
+        x(2) = sensor(iii,2)+r
+        do ii = 1, 4*r
+            if (ii.le.r.or.ii.gt.3*r) then
+                x(1) = x(1) + 1
+            else
+                x(1) = x(1) - 1
+            end if
+            if (ii.le.2*r) then
+                x(2) = x(2) - 1
+            else
+                x(2) = x(2) + 1
+            end if
+            print *,x
 
-    close(1)
+            is_distress = .true.
+            if (any(x.lt.0).or.any(x.gt.4000000)) then
+                is_distress=.false.
+                cycle
+            end if
+
+            do i = 1,num_sensors
+                if (i.eq.iii) cycle
+                if (manhattan(sensor(i,:),x).le.distances(i)) then
+                    is_distress = .false.
+                    exit
+                end if
+            end do
+            if (is_distress) exit
+        end do
+        if (is_distress) exit
+    end do
+    print *,"-------------------------------------------------------------------------"
+    print *,x
+    print *,4000000*x(1) + x(2)
+    print *,"I can't bring myself to change the integer types to allow this calculation"
+
 end program day15
