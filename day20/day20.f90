2233675dea9e877363e37c41d959429bac95ddcc
Ormorod <Adam.Ormondroyd@gmail.com>
2022-12-21 15:09:52 +0000

    day 20b. Should probably cut down on the kind=8 integers...

--- day20/day20.f90
+++ day20/day20.f90
@@ -1,7 +1,7 @@
 function wrap(a, b)
     ! essentially a 1-indexed modulo
-    integer :: a, b
-    integer :: wrap
+    integer(kind=8) :: a, b
+    integer(kind=8) :: wrap
     if (a.gt.0) then
         wrap = modulo(a,b-1)
     else
@@ -10,37 +10,41 @@ function wrap(a, b)
         
 end function
 
-subroutine day20a(x, num_lines)
+subroutine day20b(w, num_lines)
     implicit none
-    integer :: num_lines
-    integer, dimension(0:num_lines-1) :: x, idx, y
-    integer :: i,ii,xi,delta
-    integer :: old_idx,new_idx
-    integer :: wrap
+    integer(kind=8) :: num_lines
+    integer(kind=8), dimension(0:num_lines-1) :: w, idx
+    integer(kind=8) :: i,ii,iii,xi
+    integer(kind=8) :: old_idx,new_idx
+    integer(kind=8) :: wrap
+    integer(kind=8), dimension(0:num_lines-1) :: x, y
 
     do i=0,num_lines-1
         idx(i)=i
+        x(i) = w(i)
+        x(i) = x(i) * 811589153
     end do
 
-    do i=0,num_lines-1
-        print *,"here"
-        xi = x(i)
-        print *,xi
-        if (xi.eq.0) cycle
-        old_idx = idx(i)
-        new_idx = wrap(xi+idx(i),num_lines)
-        print *,new_idx
+    print *,x
 
-        do ii=0,num_lines-1
-            if (old_idx.lt.new_idx) then
-                if (idx(ii).gt.old_idx.and.idx(ii).le.new_idx) idx(ii) = idx(ii)-1
-            else
-                if (idx(ii).ge.new_idx.and.idx(ii).lt.old_idx) idx(ii) = idx(ii)+1
-            end if
+    do iii=1,10
+        do i=0,num_lines-1
+            xi = x(i)
+            if (xi.eq.0) cycle
+            old_idx = idx(i)
+            new_idx = wrap(xi+idx(i),num_lines)
+
+            do ii=0,num_lines-1
+                if (old_idx.lt.new_idx) then
+                    if (idx(ii).gt.old_idx.and.idx(ii).le.new_idx) idx(ii) = idx(ii)-1
+                else
+                    if (idx(ii).ge.new_idx.and.idx(ii).lt.old_idx) idx(ii) = idx(ii)+1
+                end if
+            end do
+
+            idx(i) = new_idx
         end do
-
-        idx(i) = new_idx
- 
+     
         do ii=0,num_lines-1
             y(idx(ii)) = x(ii)
         end do
@@ -53,6 +57,8 @@ subroutine day20a(x, num_lines)
     do i=0,num_lines-1
         if (y(i).eq.0) exit
     end do
+
+    print *,"um"
     print *,y(modulo(1000+i,num_lines))+y(modulo(2000+i,num_lines))+y(modulo(3000+i,num_lines))
 
     
@@ -61,9 +67,9 @@ end subroutine
 
 program day20
     implicit none
-    integer, dimension(:), allocatable :: x
-    integer :: num_lines, i
-    integer :: wrap
+    integer(kind=8), dimension(:), allocatable :: x
+    integer(kind=8) :: num_lines, i
+    integer(kind=8) :: wrap
     
     open(unit=1,file="input.txt")
 
@@ -88,6 +94,6 @@ program day20
         print *,wrap(x(i)+i, num_lines)
     end do
 
-    call day20a(x, num_lines)
+    call day20b(x, num_lines)
     
 end program 
